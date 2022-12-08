8f010ad3104004eb8ea7535cf864a6efcda3633c
dj-algorhythm <6372908+dj-algorhythm@users.noreply.github.com>
2022-12-08 21:32:29 +0000

    modified normdate.sh

--- normdate.sh
+++ normdate.sh
@@ -7,12 +7,12 @@ monthNumToName()
 {
     # Sets the 'month' variable to the appropriate value.
     case $1 in
-        1 ) month="Jan"     ;;  2 ) month="Feb"     ;;
-        3 ) month="Mar"     ;;  4 ) month="Apr"     ;;
-        5 ) month="May"     ;;  6 ) month="Jun"     ;;
-        7 ) month="Jul"     ;;  8 ) month="Aug"     ;;
-        9 ) month="Sep"     ;;  10) month="Oct"     ;;
-        11) month="Nov"     ;;  12) month="Dec"     ;;
+        "1" ) month="Jan"     ;;  "2" ) month="Feb"     ;;
+        "3" ) month="Mar"     ;;  "4" ) month="Apr"     ;;
+        "5" ) month="May"     ;;  "6" ) month="Jun"     ;;
+        "7" ) month="Jul"     ;;  "8" ) month="Aug"     ;;
+        "9" ) month="Sep"     ;;  "10") month="Oct"     ;;
+        "11") month="Nov"     ;;  "12") month="Dec"     ;;
         * ) echo "$0: Unknown month value $1" >&2
             exit 1
     esac
@@ -36,7 +36,7 @@ if [ "$3" -le 99 ] ; then
 fi
 
 # Is month input format a number?
-if [ -z $( echo "$1" | sed 's/[[:digit:]]//g') ]; then         # echo "$1" | sed 's/[[:digit:]]//g'
+if [ -z $( echo $1 | sed 's/[[:digit:]]//g') ] ; then         # echo "$1" | sed 's/[[:digit:]]//g'
     monthNumToName "$1"
 else
     # Normalize to first 3 letters, first upper- and then lowercase.
